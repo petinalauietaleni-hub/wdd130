528f675f7ed6c208d1881e37796c8150687d8744
Petina Lauie Taleni <petinalauietaleni@gmail.com>
2025-10-03 10:45:04 +1300

    Create numbers_activity.py
    W05 Code-along Activity: Lists of Numbers

---
+++ numbers_activity.py
@@ -0,0 +1,12 @@
+numbers = []
+
+user_number = -1
+
+while user_number != 0:
+    # ask the user for a number
+    user_number = int(input("Please enter a number"))
+
+    #put it into the list
+    numbers.append(user_number)
+
+print(numbers)
